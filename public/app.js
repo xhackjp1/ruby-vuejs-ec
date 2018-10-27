33ca4d98dd7f221e5580b48af7d64c0e2eea859b
松田信介 <matsusin0413@gmail.com>
2018-10-27 19:46:12 +0900

    アイテム修正

--- public/app.js
+++ public/app.js
@@ -1,10 +1,4 @@
-const NYTBaseUrl = "https://api.nytimes.com/svc/topstories/v2/";
-const ApiKey = "39dddc2594204a0d8f902d387200855b";
-const SECTIONS = "home, arts, automobiles, books, business, fashion, food, health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, realestate, science, sports, sundayreview, technology, theater, tmagazine, travel, upshot, world"; // From NYTimes
-
-function buildUrl(url) {
-  return NYTBaseUrl + url + ".json?api-key=" + ApiKey
-}
+const SECTIONS = "white, red, blue, grees, pink";
 
 Vue.component('item-list', {
   props: ['items'],
@@ -27,80 +21,30 @@ Vue.component('item-list', {
   `,
 });
 
-Vue.component('news-list', {
-  props: ['results'],
-  template: `
-    <section>
-      <div class="row" v-for="posts in processedPosts">
-        <div class="columns large-3 medium-6" v-for="post in posts">
-          <div class="card">
-            <div class="card-divider">
-              {{ post.title }}
-            </div>
-            <a :href="post.url" target="_blank"><img :src="post.image_url"></a>
-            <div class="card-section">
-              <p>{{ post.abstract }}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  `,
-  computed: {
-    processedPosts() {
-      let posts = this.results;
-
-      // Add image_url attribute
-      posts.map(post => {
-        let imgObj = post.multimedia.find(media => media.format === "superJumbo");
-        post.image_url = imgObj ? imgObj.url : "http://placehold.it/300x200?text=N/A";
-      });
-
-      // Put Array into Chunks
-      let i, j, chunkedArray = [],
-        chunk = 4;
-      for (i = 0, j = 0; i < posts.length; i += chunk, j++) {
-        chunkedArray[j] = posts.slice(i, i + chunk);
-      }
-      return chunkedArray;
-    }
-  }
-});
-
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [],
     items: [
       {title: "タイトル1", price: 2000, url: "", image_url: "./images/sample1.png"},
       {title: "タイトル2", price: 2400, url: "", image_url: "./images/sample2.png"},
       {title: "タイトル3", price: 2600, url: "", image_url: "./images/sample3.png"},
-      {title: "タイトル4", price: 2000, url: "", image_url: "./images/sample1.png"},
-      {title: "タイトル5", price: 2400, url: "", image_url: "./images/sample2.png"},
-      {title: "タイトル6", price: 2600, url: "", image_url: "./images/sample3.png"},
-      {title: "タイトル7", price: 2000, url: "", image_url: "./images/sample1.png"},
-      {title: "タイトル8", price: 2400, url: "", image_url: "./images/sample2.png"},
-      {title: "タイトル9", price: 2600, url: "", image_url: "./images/sample3.png"},
+      {title: "タイトル4", price: 2000, url: "", image_url: "./images/sample4.png"},
+      {title: "タイトル5", price: 2400, url: "", image_url: "./images/sample5.png"},
+      {title: "タイトル6", price: 2600, url: "", image_url: "./images/sample6.png"},
+      {title: "タイトル7", price: 2000, url: "", image_url: "./images/sample7.png"},
+      {title: "タイトル8", price: 2400, url: "", image_url: "./images/sample8.png"},
+      {title: "タイトル9", price: 2600, url: "", image_url: "./images/sample9.png"},
     ],
     sections: SECTIONS.split(', '), // create an array of the sections
-    section: 'home', // set default section to 'home'
+    section: 'white', // set default section to 'home'
     loading: true,
     title: ''
   },
   mounted() {
-    this.getPosts('home');
+    //this.getPosts('home');
   },
   methods: {
-    getPosts(section) {
-      let url = buildUrl(section);
-      axios.get(url).then((response) => {
-        this.loading = false;
-        this.results = response.data.results;
-        let title = this.section !== 'home' ? "Top stories in '" + this.section + "' today" : "Top stories today";
-        this.title = title + "(" + response.data.num_results + ")";
-      }).catch(error => {
-        console.log(error);
-      });
+    getItems(section) {
     }
   }
 });
