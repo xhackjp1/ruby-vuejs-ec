141ac3a869ca0deab5e98508de1c69b9dacd8be1
松田 信介 <matsusin0413@gmail.com>
2018-12-09 11:28:18 +0900

    payjpの組み込み

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra"
+gem "payjp"
 
 # DB追加のためのgem
 gem "rake"
--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,41 @@
 # myapp.rb
 require 'sinatra'
 require "sinatra/activerecord"
+require 'payjp'
 require './models/item.rb'
+
+
+set :secret_key, ENV['SECRET_KEY']
+set :public_key, ENV['PUBLIC_KEY']
+
+Payjp.api_key = settings.secret_key
+
+configure do
+  set :erb, escape_html: true
+end
 
 get '/' do
   erb :index
+end
+
+get '/card' do
+  erb :card
+end
+
+post '/pay' do
+  puts "支払い"
+  amount = 1000
+  customer = Payjp::Customer.create(
+    :email => 'example@pay.jp',
+    :card  => params['payjp-token']
+  )
+  Payjp::Charge.create(
+      :amount => amount,
+      :currency => 'jpy',
+      :customer => customer.id,
+      :description => 'Sinatra example charge'
+  )
+  erb :pay, :locals => {:amount => amount}
 end
 
 get '/index' do
